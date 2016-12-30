9f7fa60973dda1aa56934d5586f2ab3e012e4fa3
lbenychou <lbenychou@egencia.com>
2016-12-30 10:55:41 +0100

    Message Xtreme support

--- src/org/toilelibre/libe/athg2sms/business/pattern/BuiltInFormatName.java
+++ src/org/toilelibre/libe/athg2sms/business/pattern/BuiltInFormatName.java
@@ -2,7 +2,7 @@ package org.toilelibre.libe.athg2sms.business.pattern;
 
 
 public enum BuiltInFormatName {
-    MxtSmsglobal("Message Xtreme Gateway"), NokiaSuite ("Nokia Suite 3.8"), NokiaVmgInbox ("Nokia Vmg Inbox"), NokiaVmgSent ("Nokia Vmg Sent"), NokiaCsv ("Nokia Csv"), IPhoneCsv ("iPhone Csv"), BlackberryCsv ("Blackberry Csv"), DateAndFromAndAddressAndbody ("Date+'from'+address+body"),
+    MxtSmsglobal("Message Xtreme SMSGlobal"), NokiaSuite ("Nokia Suite 3.8"), NokiaVmgInbox ("Nokia Vmg Inbox"), NokiaVmgSent ("Nokia Vmg Sent"), NokiaCsv ("Nokia Csv"), IPhoneCsv ("iPhone Csv"), BlackberryCsv ("Blackberry Csv"), DateAndFromAndAddressAndbody ("Date+'from'+address+body"),
     DateAndAddressAndBodyAndINBOX ("Date+address+body+INBOX"), NokiaCsvWithQuotes ("Nokia Csv with quotes"), NokiaCsvWithCommas ("Nokia Csv with commas"), UnknownSmsFormat1 ("Unknown sms format 1"), LumiaVmg ("Lumia Vmg");
 
     private final String value;
